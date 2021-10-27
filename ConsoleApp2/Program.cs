4a6f2d13e9ba2625ac1d39110ba9d460e02c1bb6
MosabRM <92996115+MosabRM@users.noreply.github.com>
2021-10-27 15:14:48 +0500

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Power Formula 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("P value  :");
+            decimal P = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("T value  :");
+            decimal T = Convert.ToDecimal(Console.ReadLine());
+            decimal result = P * T;
+            Console.WriteLine(" W = " + result);
+        }
+    }
+}
